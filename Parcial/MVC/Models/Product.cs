4a6a75987a5ea83e6eea7c7bfb210c3a7a441b18
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-04-05 23:29:45 -0400

    Commit Modelo MVC

---
+++ Parcial/MVC/Models/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        caja
+    } 
+
+      public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+
+
+    public class Product
+    {
+
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required]
+
+        public string  Nombre { get; set; }
+
+        
+        public decimal Precio { get; set; }
+
+        
+        public int Cantidad { get; set; }
+
+        [Required]
+
+        public TipoUnidad Quantity { get; set; }
+
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+
+    }
+}
