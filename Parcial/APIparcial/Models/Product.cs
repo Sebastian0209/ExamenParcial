660da774a5011a612b797699c8e460e8b7ef3c3c
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-04-05 23:39:51 -0400

    Commit WebApi

---
+++ Parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIparcial.Models
+{
+
+    public enum TipoUnidad
+    {
+        Botella,
+        Metro,
+        Litro,
+        caja
+    } 
+
+      public enum TipoEstado
+    {
+        Activo,
+        Inactivo
+    }
+
+
+    public class Product
+    {
+
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required]
+
+        public string  Nombre { get; set; }
+
+        
+        public decimal Precio { get; set; }
+
+        
+        public int Cantidad { get; set; }
+
+        [Required]
+
+        public TipoUnidad Quantity { get; set; }
+
+        [Required]
+        public TipoEstado Estado { get; set; }
+
+
+    }
+}
